f5c37b0bd3e3cb57a993d8a113aee17845456176
Nick Henderson <nch8501@rit.edu>
2018-11-08 10:12:36 -0500

    login and signup redirect to chat page

--- server/router.js
+++ server/router.js
@@ -4,12 +4,12 @@ const mid = require('./middleware');
 // routes to go to
 const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
-  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
+  // app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  // app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
 
 
   app.get('/chat', mid.requiresLogin, controllers.Chat.chatPage);
@@ -17,8 +17,8 @@ const router = (app) => {
   app.post('/makeChat', mid.requiresLogin, controllers.Chat.makeChat);
 
 
-  app.post('/maker', mid.requiresLogin, controllers.Domo.make);
-  app.delete('/deleteDomo', mid.requiresLogin, controllers.Domo.delete);
+  // app.post('/maker', mid.requiresLogin, controllers.Domo.make);
+  // app.delete('/deleteDomo', mid.requiresLogin, controllers.Domo.delete);
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
