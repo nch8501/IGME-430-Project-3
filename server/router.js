d2e1d7a1bdb6c2f958cdd26608a765fa7e7403e3
Nick Henderson <nch8501@rit.edu>
2018-11-18 15:18:45 -0500

    Pages not found redirects to login or chat page

--- server/router.js
+++ server/router.js
@@ -23,6 +23,7 @@ const router = (app) => {
   app.post('/updatePassword', mid.requiresLogin, controllers.Account.updatePassword);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/*', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
